50c7c9bcb3f059039eaa32abaa67b7f0b91647ec
Andrey Berdnik <AndreyBerdnik@gmail.com>
2021-07-15 20:53:53 +0300

    Lesson 01 change spaces

--- Lesson_01/main.c
+++ Lesson_01/main.c
@@ -11,15 +11,15 @@ Code, Compile, Run and Debug online from anywhere in world.
 int
 main ()
 {
-  printf ("--------------------------------------------------------\n");
-  printf ("|%-31s|%10s|%11s|\n","CPU","Rate","Price");
-  printf ("--------------------------------------------------------\n");
-  printf ("|%-31s| %8i |%11.2f|  \n", "AMD Athlon II N370 Dual-Core", 899, 0);
-  printf ("|%-31s| %8i |%11.2f|  \n","AMD Athlon II N370 Dual-Core", 899, 0);
-  printf ("|%-31s| %8i |%11.2f|  \n","AMD Athlon II X2 240", 898, 24.5);
-  printf ("|%-31s| %8i |%11.2f|  \n","Intel Pentium G620T @ 2.20GHz", 896, 19.99);
-  printf ("|%-31s| %8i |%11.2f|  \n","Qualcomm MSM8937", 892, 0);
-  printf ("--------------------------------------------------------\n");
+  printf ("------------------------------------------------------\n");
+  printf ("|%-31s|%8s|%11s|\n","CPU","Rate","Price");
+  printf ("------------------------------------------------------\n");
+  printf ("|%-31s|%8i|%11.2f|\n", "AMD Athlon II N370 Dual-Core", 899, 0.0);
+  printf ("|%-31s|%8i|%11.2f|\n", "AMD Athlon II N550", 852, 0.0);
+  printf ("|%-31s|%8i|%11.2f|\n", "AMD Athlon II X2 240", 898, 24.5);
+  printf ("|%-31s|%8i|%11.2f|\n", "Intel Pentium G620T @ 2.20GHz", 896, 19.99);
+  printf ("|%-31s|%8i|%11.2f|\n","Qualcomm MSM8937", 892, 0.0);
+  printf ("------------------------------------------------------\n");
 
   return 0;
 }
